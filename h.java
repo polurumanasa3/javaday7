afd42d2c6d1aa40937d0d0b2e6c17c68db88f060
polurumanasa3 <manasap142@gmail.com>
2024-12-08 01:53:45 -0800

    Add files via upload

---
+++ h.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class h
+{
+    public static void main(String args[])
+    {
+        int a[]={11,2,3,4,3};
+        int s=0;
+        for(int i=0;i<a.length;i++)
+        {
+            s+=a[i];
+            int res=s/(i+1);
+            a[i]=res;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
+
+        
